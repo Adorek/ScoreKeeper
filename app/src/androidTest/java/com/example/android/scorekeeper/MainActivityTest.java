ff7fd06a9a73bca5c58cab1ef166bd1046af0edc
Adorek <adam.rafinski@gmail.com>
2019-11-04 16:52:10 +0100

    Added a simple Unit Test

---
+++ app/src/androidTest/java/com/example/android/scorekeeper/MainActivityTest.java
@@ -0,0 +1,40 @@
+package com.example.android.scorekeeper;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch(){
+
+        View view = mActivity.findViewById(R.id.player_one_score);
+
+        assertNotNull(view);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        mActivity = null;
+    }
+}
